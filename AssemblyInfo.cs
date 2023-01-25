48b0af4f0c72cb103c0bcee60586a97bcba71e6a
Tine Staric <tine.staric@companial.com>
2023-01-25 10:18:38 +0200

    Added explicit assemblyInfo

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+//[assembly: Extension]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("0.01")]
+[assembly: AssemblyInformationalVersion("0.01")]
+[assembly: AssemblyVersion("0.01")]

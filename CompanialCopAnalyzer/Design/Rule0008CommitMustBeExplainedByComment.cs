b3c79b948d17f8a90e2c4194f7fd6846a538c938
Audrius Razma <audrius.razma@companial.com>
2023-10-03 06:52:23 +0000

    Merged PR 14900: Fixed where rule wont show commit without parenthesis
    Fixed where rule wont show commit without parenthesis

--- CompanialCopAnalyzer/Design/Rule0008CommitMustBeExplainedByComment.cs
+++ CompanialCopAnalyzer/Design/Rule0008CommitMustBeExplainedByComment.cs
@@ -15,21 +15,25 @@ namespace CompanialCopAnalyzer.Design
     {
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0008CommitMustBeExplainedByComment);
 
-        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeCommitExpression, SyntaxKind.InvocationExpression);
+        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeCommitExpression, SyntaxKind.ExpressionStatement);
 
         public void AnalyzeCommitExpression(SyntaxNodeAnalysisContext context)
         {
             if (UpgradeVerificationHelper.IsObsoleteOrDeprecated(context.ContainingSymbol)) return;
             if (UpgradeVerificationHelper.IsObsoleteOrDeprecated(context.ContainingSymbol.GetContainingObjectTypeSymbol())) return;
 
-            IBuiltInMethodTypeSymbol? builtInMethodTypeSymbol = context.SemanticModel.GetSymbolInfo(context.Node).Symbol as IBuiltInMethodTypeSymbol;
+            ExpressionStatementSyntax? expression = context.Node as ExpressionStatementSyntax;
+
+            if(expression == null) return;
+
+            IBuiltInMethodTypeSymbol? builtInMethodTypeSymbol = context.SemanticModel.GetSymbolInfo(expression.Expression).Symbol as IBuiltInMethodTypeSymbol;
             if(builtInMethodTypeSymbol?.Name != "Commit" && builtInMethodTypeSymbol?.ContainingSymbol?.Name != "Database")
             {
                 return;
             }
 
-            if (context.Node.Parent.GetLeadingTrivia().Any(x => x.IsKind(SyntaxKind.LineCommentTrivia))) return;
-            if (context.Node.Parent.GetTrailingTrivia().Any(x => x.IsKind(SyntaxKind.LineCommentTrivia))) return;
+            if (context.Node.GetLeadingTrivia().Any(x => x.IsKind(SyntaxKind.LineCommentTrivia))) return;
+            if (context.Node.GetTrailingTrivia().Any(x => x.IsKind(SyntaxKind.LineCommentTrivia))) return;
 
             context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0008CommitMustBeExplainedByComment, context.Node.GetLocation()));
         }
